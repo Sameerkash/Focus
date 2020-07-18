bfda19eb5784c3fbf2dbfb1a4ba2b8081c4eb508
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-07-18 13:07:20 +0530

    added basic file structure

---
+++ lib/core/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static final theme = ThemeData(
+    primaryColor: Colors.grey[600],
+    cardColor: Colors.black,
+    scaffoldBackgroundColor: Colors.grey[700],
+  );
+}
